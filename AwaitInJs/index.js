6a0bccb38b63b3628cd1e7b52f6e323d010ecd10
gitshubhsss <shubhamranjane16@gmail.com>
2024-04-02 17:06:35 +0530

    async await

--- AwaitInJs/index.js
+++ AwaitInJs/index.js
@@ -1,33 +1,28 @@
 let nums = document.querySelector(".nums");
-let numArr = [
-  "O",
-  "M",
-  "K",
-  "A",
-  "R",
-  "D",
-  "A",
-  "S",
-  "W",
-  "A",
-  "D",
-  "K",
-  "A",
-  "R",
-];
-
-async function genRandNum(char) {
+let name= prompt("Enter your name");
+let numArr=name.split('');
+console.log(numArr);
+async function genRandNum(char,randColor) {
     return new Promise((resolve,reject) => {
       setTimeout(() => {
         nums.innerText = char;
+        nums.style.color=randColor();
+        console.log(randColor);
         resolve();
-      }, 1000);
+      }, 500);
     });
   }
   
 async function randNum() {
   for(char of numArr){
-    await genRandNum(char);
+    await genRandNum(char,randColor);
   }
+ 
 }
 randNum();
+let colors=["red","blue","orange","green","purple","pink","skyblue","darkgreen","white"]
+function randColor(){
+  let idx=Math.floor(Math.random()*4);
+  let randColor=colors[idx];
+  return randColor;
+}
